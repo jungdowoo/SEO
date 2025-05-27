b07bcff06a1265ab9028fb6d308757b0cadad1f2
jungdowoo <jdw9302@naver.com>
2025-05-27 12:59:42 +0900

    tetet

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,3 +4,5 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
+console.log('[ENV]', process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log('[ENV]', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.slice(0, 30));
