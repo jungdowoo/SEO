671e778133a4dee5ba7e92e45bd9fc7626712f6b
jungdowoo <jdw9302@naver.com>
2025-05-30 10:45:46 +0900

    임시2

---
+++ app/blog/[id]/metadata.ts
@@ -0,0 +1,23 @@
+import { Metadata } from "next";
+
+type Params = { params: { id: string } };
+
+export const generateMetadata = async ({ params }: Params): Promise<Metadata> => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/metadata/${params.id}`, {
+    next: { revalidate: 60 },
+  });
+
+  const post = await res.json();
+
+  if (!post?.title) {
+    return {
+      title: "글을 찾을 수 없습니다",
+      description: "존재하지 않는 게시글입니다.",
+    };
+  }
+
+  return {
+    title: `${post.title} | 썰레발`,
+    description: post.content.slice(0, 100) + "...",
+  };
+};
